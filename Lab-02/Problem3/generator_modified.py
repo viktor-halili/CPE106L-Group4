989f6c9672f94033de067b4d56bdc5e171fef790
viktor-halili <vaehalili@mymail.mapua.edu.ph>
2025-09-24 23:41:59 +0800

    Edsel Lee Contribution

---
+++ Lab-02/Problem3/generator_modified.py
@@ -0,0 +1,32 @@
+
+articles = getWords("articles.txt")
+nouns = getWords("nouns.txt")
+verbs = getWords("verbs.txt")
+prepositions = getWords("prepositions.txt")
+
+def sentence():
+    """Builds and returns a sentence."""
+    return nounPhrase() + " " + verbPhrase()
+
+def nounPhrase():
+    """Builds and returns a noun phrase."""
+    return random.choice(articles) + " " + random.choice(nouns)
+
+def verbPhrase():
+    """Builds and returns a verb phrase."""
+    return random.choice(verbs) + " " + nounPhrase() + " " + \
+           prepositionalPhrase()
+
+def prepositionalPhrase():
+    """Builds and returns a prepositional phrase."""
+    return random.choice(prepositions) + " " + nounPhrase()
+
+def main():
+    """Allows the user to input the number of sentences to generate."""
+    number = int(input("Enter the number of sentences: "))
+    for count in range(number):
+        print(sentence())
+
+# Entry point
+if _name_ == "_main_":
+    main()
